665377a786788194a3fe1a9afb8c1d32cae87314
Javier Fraga García <javierfrga@proton.me>
2026-09-23 12:14:01 +0200

    Add Consumer class with asynchronous audio consumption method

---
+++ src/consumer.py
@@ -0,0 +1,17 @@
+import asyncio
+
+class Consumer:
+
+    @staticmethod
+    async def consume(audio_queue: asyncio.Queue, text_queue: asyncio.Queue):
+        while True:
+            chunks = []
+            while True:
+                chunk = await audio_queue.get()
+                if chunk is None:
+                    audio_queue.task_done()
+                    break
+                audio_queue.task_done()
+                chunks.append(chunk)
+
+            await text_queue.put(chunks)
